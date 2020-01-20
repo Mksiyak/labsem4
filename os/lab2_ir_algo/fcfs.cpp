e03bbd38098b1523284b6ac1cc00fa3fcf58b697
Mksiyak <mksiyak@mksiyak-Inspiron-3576mukeshsiyak002@gmail.com>
2020-01-21 05:08:21 +0530

    os lab

---
+++ os/lab2_ir_algo/fcfs.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define deb(x) cout<<#x<<' '<<x<<endl;
+int main()
+{
+	int no_of_process,no_of_total_cycles_used=0;
+
+	cout<<"enter number of processes : ";
+	
+	cin>>no_of_process;
+	
+	tuple<int,int,int>t[no_of_process];
+	
+	for (int i = 0; i < no_of_process; ++i)
+	{
+	
+		int pid,brust_time,arrival_time;
+	
+		cout<<"enter pid (INT) :";
+	
+		cin>>pid;
+	
+		cout<<"enter brust time for pid "<<pid<<" : ";
+	
+		cin>>brust_time;
+		
+		cout<<"enter arrival time for pid "<<pid<<" : ";
+	
+		cin>>arrival_time;
+	
+		t[i] = make_tuple(arrival_time,brust_time,pid);
+	
+	}
+	
+	int total_waiting_time = 0,cycle_timer = 0;
+	
+	sort(t,t+no_of_process);
+	
+	std::map<int, int> tat,wt;
+	
+	for (int i = 0; i < no_of_process; ++i)
+	{
+		cycle_timer=max(cycle_timer,get<0>(t[i]))+get<1>(t[i]);
+
+		deb(cycle_timer)
+	
+		tat[get<2>(t[i])] = cycle_timer-get<0>(t[i]);
+	
+		wt[get<2>(t[i])] = cycle_timer-get<0>(t[i])-get<1>(t[i]);
+	
+		total_waiting_time += wt[get<2>(t[i])];
+	
+		deb(total_waiting_time);
+	
+		cout<<"for pid "<<get<2>(t[i])<<"tat is "<<tat[get<2>(t[i])]<<" and waiting time is "<<wt[get<2>(t[i])]<<endl;
+	}
+
+	cout<<"avg waiting time is : "<<(double)total_waiting_time/no_of_process<<endl;
+}
+/*
+5
+0 1 3
+1 4 1
+2 2 4
+3 6 0
+4 3 2
+*/
---
+++ os/lab2_ir_algo/sjf_preemptive.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define deb(x) cout<<#x<<' '<<x<<endl;
+
+bool sortbysec(const tuple<int, int, int>& a,  
+               const tuple<int, int, int>& b) 
+{ 
+    return (get<1>(a) < get<1>(b)); 
+}
+
+bool sortbyth(const tuple<int, int, int>& a,  
+              const tuple<int, int, int>& b) 
+{ 
+    return (get<2>(a) < get<2>(b)); 
+} 
+int main()
+{
+	int no_of_process,no_of_total_cycles_used=0;
+
+	cout<<"enter number of processes : ";
+	
+	cin>>no_of_process;
+	
+	tuple<int,int,int>t[no_of_process];
+
+	std::map<int, int> originalBt;
+	
+	for (int i = 0; i < no_of_process; ++i)
+	{
+	
+		int pid,brust_time,arrival_time;
+	
+		cout<<"enter pid (INT) :";
+	
+		cin>>pid;
+	
+		cout<<"enter brust time for pid "<<pid<<" : ";
+	
+		cin>>brust_time;
+		
+		cout<<"enter arrival time for pid "<<pid<<" : ";
+	
+		cin>>arrival_time;
+	
+		t[i] = make_tuple(brust_time,arrival_time,pid);
+
+		originalBt[pid] = brust_time;
+	}
+	
+	sort(t,t+no_of_process,sortbysec);
+
+	no_of_total_cycles_used=0;
+	
+	for (int i = 0; i < no_of_process; ++i)
+	{
+		no_of_total_cycles_used=max(no_of_total_cycles_used,get<0>(t[i]))+get<1>(t[i]);
+	}
+
+	int total_waiting_time = 0,cycle_timer = 0;
+		
+	std::map<int, int> tat,wt;
+	
+	for (int i = 0; i < no_of_total_cycles_used; ++i)
+	{
+		sort(t,t+no_of_process);
+		
+		int j=0;
+
+		while(1)
+		{
+			if(j<no_of_process and get<0>(t[j])!=0 and get<1>(t[j])<=i)
+			{
+				get<0>(t[j])--;
+				break;
+			}
+			j++;
+		}
+		
+		if(j!=no_of_process and  get<0>(t[j])==0)
+		{
+			tat[get<2>(t[j])]=i+1-get<1>(t[j]);
+
+			wt[get<2>(t[j])]=tat[get<2>(t[j])]-originalBt[get<2>(t[j])];
+
+	    	total_waiting_time += wt[get<2>(t[j])];
+
+			cout<<"for pid "<<get<2>(t[j])<<" tat is "<<tat[get<2>(t[j])]<<" and waiting time is "<<wt[get<2>(t[j])]<<endl;
+		}
+	}
+
+	cout<<"avg waiting time is : "<<(double)total_waiting_time/no_of_process<<endl;
+}
