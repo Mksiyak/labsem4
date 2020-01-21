c4cb654b08b394cd76d4395f703252881015fc91
Mksiyak <mksiyak@mksiyak-Inspiron-3576mukeshsiyak002@gmail.com>
2020-01-21 14:46:31 +0530

    c

---
+++ os/lab2_ir_algo/robin_round.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define deb(x) cout<<#x<<' '<<x<<endl;
+int main()
+{
+	int no_of_process,no_of_total_cycles_used=0,robine_time;
+
+	cout<<"enter number of processes : ";
+	
+	cin>>no_of_process;
+	
+	tuple<int,int,int>t[no_of_process];
+
+	cout<<"enter time";
+
+	cin>>robine_time;
+	
+	for (int i = 0; i < no_of_process; ++i)
+	{
+	
+		int pid,brust_time,arrival_time;
+	
+		cout<<"enter pid (INT) :";
+	
+		cin>>pid;
+	
+		cout<<"enter brust time for pid "<<pid<<" : ";
+	
+		cin>>brust_time;
+		
+		cout<<"enter arrival time for pid "<<pid<<" : ";
+	
+		cin>>arrival_time;
+	
+		t[i] = make_tuple(arrival_time,pid,brust_time);
+	
+	}
+	
+	int total_waiting_time = 0,cycle_timer = 0;
+	
+	sort(t,t+no_of_process);
+	
+	std::map<int, int> tat,wt;
+	
+	int itr = 0,clock=0;
+
+	queue<tuple<int,int,int>>robine_que;
+	while(1)
+	{
+		int cycle_time = robine_time;
+		while(itr<no_of_process and cycle_timer >=get<1>(t[itr]))
+		{
+			robine_que.push(t[itr]);
+			deb(get<2>(t[itr]));
+			itr++;
+		}
+		if(robine_que.size()==0 and itr >= no_of_process)break;
+		while(robine_que.size()==0 and get<2>(robine_que.front())<=cycle_time)
+		{
+			cycle_timer += get<2>(robine_que.front());
+			int tat = cycle_timer-get<0>(robine_que.front());
+			int wt = tat - get<2>(robine_que.front());
+		    cout<<"for pid "<<get<1>(robine_que.front())<<"tat is "<<tat<<" and waiting time is "<<wt<<endl;
+		    cycle_time -= get<2>(robine_que.front());
+		    robine_que.pop();
+		    deb(get<1>(robine_que.front()))
+		}
+		if(cycle_time>0)
+		{
+			auto temp = robine_que.front();
+			robine_que.pop();
+			get<2>(temp)-=cycle_time;
+			robine_que.push(temp);
+			cycle_timer+=cycle_time;
+		}
+	}
+}
+/*
+5
+10
+0 10 0
+1 29 0
+2 3 0
+3 7 0
+4 12 0
+*/
