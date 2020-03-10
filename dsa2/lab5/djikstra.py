a59b05bb6b282b4d007def62cb2d3d3cf03cc790
Mksiyak <mksiyak@mksiyak-Inspiron-3576mukeshsiyak002@gmail.com>
2020-03-11 02:41:57 +0530

    hjggt

--- dsa2/lab5/djikstra.py
+++ dsa2/lab5/djikstra.py
@@ -1,17 +1,29 @@
-import collections
-class heap():
-    def __init__(self,lst):
-        self.hep = [(0,0)]*len(lst)
-        self.ref = self.hep
-        for i in range(len(lst)):
-            self.hep[i][0] = i
-            self.hep[i][1] = 1000000000
-        
+import heapq
+class Node:
+    def __init__(self,key):
+        self.key =  key
+        self.weight = 55555555555555
+    def __lt__(self,other):
+        return self.weight<other.weight
 class graph():
-    def __init__(self,vertices):
-        self.v = vertices
+    def __init__(self,adjlist):
+        self.adjlist = adjlist
     def djikstra(self,src):
-        pass
+        heap = []
+        for i in range(len(self.adjlist)):
+            heap.append(Node(i))
+        mp = {}
+        for i in range(len(heap)):
+            mp[i] = heap[i]
+        mp[src].weight = 0
+        while(len(heap)>0):
+            src = heapq.heappop(heap).key
+            for i in self.adjlist[src]:
+                if(mp[i[0]].weight>mp[src].weight+i[1]):
+                    mp[i[0]].weight=mp[src].weight+i[1]
+                    heapq.heapify(heap)
+            print(src)
+        
 def main():
     G = []
     file=open('input.txt','r')
@@ -21,12 +33,13 @@ def main():
         first=True
         for edge in line.split(' '):
             if first:
-                first=False
+                first = False
                 continue
-            node,weight =  edge.split(',')
+            node, weight = edge.split(',')
             adjacentVertices.append((int(node),int(weight)))
         G.append(adjacentVertices)
     file.close()
-    hp = heap(G)
     print(G)
+    newgraph = graph(G)
+    newgraph.djikstra(0)
 main()
